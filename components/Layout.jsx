9422c0d340ca2fe306054934bb1db823973cc7ec
marina <nicolaimarina@gmail.com>
2022-12-27 11:43:30 -0500

    addin the footer

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { Box } from '@chakra-ui/react';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
  function Layout({ children }) {
   return (
@@ -14,7 +15,7 @@ import Navbar from './Navbar';
         </header>
         <main>{children}</main>
         <footer>
-          Footer
+         <Footer/>
         </footer>
       </Box>
     </>
