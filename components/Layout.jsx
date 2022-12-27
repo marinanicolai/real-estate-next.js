b9eb9cbcbeaece9065e38ec6de140c3088a779ef
marina <nicolaimarina@gmail.com>
2022-12-27 11:38:05 -0500

    adding the NavBar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { Box } from '@chakra-ui/react';
+import Navbar from './Navbar';
 
  function Layout({ children }) {
   return (
@@ -9,7 +10,7 @@ import { Box } from '@chakra-ui/react';
       </Head>
       <Box maxWidth='1280px' m='auto'>
         <header>
-          Navbar
+         < Navbar />
         </header>
         <main>{children}</main>
         <footer>
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
-import Property from "../componets/Property";
+import Property from "../components/Property";
 import { baseUrl, fetchApi } from "../utils/fetchApi";
 
 export const Banner = ({
